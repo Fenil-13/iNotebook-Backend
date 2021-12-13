5adb5cc43e6be750a41024000c4979aea490e21b
Fenil Moradiya <53660680+Fenil-13@users.noreply.github.com>
2021-12-13 23:38:23 +0530

    Create user api

--- routes/auth.js
+++ routes/auth.js
@@ -12,22 +12,30 @@ router.get('/', (req, res) => {
 router.post('/create_user',
     [body('email').isEmail(),
     body('password').isLength({ min: 6 }),],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ error: errors.array() });
         }
-        User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email
-        }).then(user => res.json({
-            userData: user,
-            status: "User Created Successfully"
-        }))
-            .catch(err => {
+        try {
+            let findUser = await User.findOne({ email: req.body.email })
+            if (findUser) {
+                return res.status(400).json({ error: "User with this email already exits" })
+            }
+            let user = await User.create({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email
+            }).catch(err => {
                 res.status(400).json({ error: err.message })
             })
+            res.json({
+                userData: user,
+                status: "User Created Successfully"
+            })
+        } catch (error) {
+            res.status(500).json({ error: error.message })
+        }
 
     }
 );
