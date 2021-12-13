85c05a22b14be968d4077f01a729fca91a1a22f7
Fenil Moradiya <53660680+Fenil-13@users.noreply.github.com>
2021-12-14 00:30:58 +0530

    Auth Token added in create user

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'FenilDevloper013'
 
 router.get('/', (req, res) => {
     res.json({
@@ -22,15 +26,21 @@ router.post('/create_user',
             if (findUser) {
                 return res.status(400).json({ error: "User with this email already exits" })
             }
+            const salt = await bcrypt.genSaltSync(10);
+            const secPass = await bcrypt.hash(req.body.password, salt)
+
+
             let user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email
             }).catch(err => {
                 res.status(400).json({ error: err.message })
             })
+            const authToken = await jwt.sign({ user: { id: user.id } }, JWT_SECRET);
+            console.log(authToken)
             res.json({
-                userData: user,
+                authToken: authToken,
                 status: "User Created Successfully"
             })
         } catch (error) {
