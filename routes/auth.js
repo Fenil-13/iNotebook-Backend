62d43c6f1e73cba5ba048deb89b41318abbcd08f
Fenil Moradiya <53660680+Fenil-13@users.noreply.github.com>
2021-12-13 20:59:30 +0530

    Add Validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
 router.get('/', (req, res) => {
     res.json({
@@ -8,12 +9,26 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/create_user', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.json({
-        status: "User Created Successfully"
-    })
-})
+router.post('/create_user',
+    [body('email').isEmail(),
+    body('password').isLength({ min: 6 }),],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        }).then(user => res.json({
+            userData: user,
+            status: "User Created Successfully"
+        }))
+            .catch(err => {
+                res.status(400).json({ error: err.message })
+            })
+
+    }
+);
 module.exports = router
